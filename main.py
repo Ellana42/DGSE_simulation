330ae0bb2cbd5262353c52dbee8d6791b8003e48
Mathilde <mtmrkaploun@gmail.com>
2020-05-26 22:44:32 +0200

    Multiple plots and correct constants

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ theta = 5
 sigma = 0.6
 heta = 0.5
 c_min = 0.1
-c_max = 1
+c_max = 5.1
 
 # variable = 0.1 phase A, 0.55 phase B, 0.75 1.05 phase C
 c_0s = [0.1, 0.55, 0.75, 1.05]
@@ -31,14 +31,23 @@ zeta_0 = 0
 current_error = zeta_0
 
 iterations = range(1000)
-consos = []
+i = 0
+
+plt.style.use('ggplot')
+fig, axs = plt.subplots(len(c_0s), 1)
+fig.suptitle('Numeric simulation')
 
 for c_0 in c_0s:
+
     current_conso = c_0
+    consos = []
+
     for _ in iterations:
-        print(current_conso, current_error)
         consos.append(log(current_conso))
         current_conso, current_error = next_conso(current_conso, current_error)
 
-    plt.plot(consos)
-    plt.show()
+    axs[i].plot(consos)
+    axs[i].set_ylabel('c0 : ' + str(c_0))
+    i += 1
+
+plt.show()
